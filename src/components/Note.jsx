b8a6683965b8ea2803bdeb5c656bd694782c185b
RajJagasia <106627812+RajJagasia@users.noreply.github.com>
2023-01-14 15:09:06 +0400

    Initial commit

---
+++ src/components/Note.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Note() {
+  return (
+    <div className="note">
+      <h1>Enter your Title</h1>
+      <p>Enter your Content </p>
+    </div>
+  );
+}
+
+export default Note;
